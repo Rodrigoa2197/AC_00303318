9c8b6b2bdd74e83a675a59fdb0721650decb6ef3
Rodrigo Arevalo Najarro <00303318@uca.edu.sv>
2021-05-11 22:20:01 -0600

    add final commit: tarea 3 ejercicio 2

--- Tarea Laboratorio 3/ejercicio2.asm
+++ Tarea Laboratorio 3/ejercicio2.asm
@@ -2,5 +2,25 @@
 
 	section	.text
 
+	xor AX, AX ;limpia AX
+	xor CX, CX ;limpia CX
+	mov CL, 5d ;inicia contador
+	mov AL, 1d ;almacena dato en AL para poder guardar aqui los datos de la multipliacion
 
-	int	20h
+	call factorial
+
+	mov CH, 1d
+	mul CH
+
+	mov [20Bh], AL ;mandamos los datos y se almacenan en esta memoria
+	jmp exit
+
+factorial:
+	mul CL ;se multiplica datos
+	sub CL, 1d ; se le resta al contador 
+	cmp CL, 0d
+	JNZ factorial; itaracion de datos para obtener el factorial
+	ret ;termina de ejectutar el Jump Not Zero
+
+exit:
+	int	20h ; fin programa
